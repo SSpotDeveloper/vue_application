81f8e51ff5df6298b7c598618abcb1bc0fca991b
Manie <subs@softspot.co.za>
2023-12-20 01:28:36 +0200

    Registration And Login Update

--- src/views/Pages/HomePage.vue
+++ src/views/Pages/HomePage.vue
@@ -1,32 +1,32 @@
 <template>
 <div class="home-background">
-    <div class="h-full flex justify-center items-center ">
-        <div class="bg-white w-1/2 h-2/6 shadow-lg rounded-lg flex divide-x divide-slate-200">
-            <div class="p-5 w-full h-full">
+    <div class="flex items-center justify-center h-full ">
+        <div class="flex w-1/2 bg-white divide-x rounded-lg shadow-lg 2xl:h-2/6 xl:h-3/6 divide-slate-200">
+            <div class="w-full h-full p-5">
                 Welcome To Manufacturing Management Systems
                 <div class="text-sm text-slate-400">
                     <h1>Status: <span class="text-green-400" v-if="ServerState">Online</span ><span v-else class="text-red-400">Offline</span></h1>
                 </div>
                 <div class="text-sm text-slate-400" v-if="!ServerState">
-                    <h1>Message: <span class="text-red-400">{{ ServerResponse.Message }}</span></h1>
+                    <h1 v-if="ServerResponse.Message">Message: <span class="text-red-400">{{ ServerResponse.Message }}</span></h1>
                 </div>
             </div>
-            <div class="p-5 w-full h-full flex items-center">
+            <div class="flex items-center w-full h-full p-5">
                 <div class="flex flex-col w-full">
                     <div>
                         <h1>Login</h1>
                     </div>
-                    <div class=" my-2 flex bg-white border p-2 border-slate-200 rounded-lg divide-x divide-slate-300">
-                        <UserIcon class="h-6 stroke-slate-300 px-2"/><input type="email" class="w-full px-1" placeholder="Email" />
+                    <div class="flex p-2 my-2 bg-white border divide-x rounded-lg border-slate-200 divide-slate-300">
+                        <UserIcon class="h-6 px-2 stroke-slate-300"/><input type="email" class="w-full px-1" placeholder="Email" />
                     </div>
-                    <div class="my-2 flex bg-white border p-2 border-slate-200 rounded-lg divide-x divide-slate-300">
-                        <LockClosedIcon class="h-6 stroke-slate-300 px-2"/><input type="password" class="w-full px-1" placeholder="Password"/>
+                    <div class="flex p-2 my-2 bg-white border divide-x rounded-lg border-slate-200 divide-slate-300">
+                        <LockClosedIcon class="h-6 px-2 stroke-slate-300"/><input type="password" class="w-full px-1" placeholder="Password"/>
                     </div>
-                    <div class="text-center mt-2">
-                        <button @click="Login" class="bg-green-300 p-2 rounded-md shadow-md hover:bg-green-600 duration-200" :disabled="!ServerState" >Login</button>
+                    <div class="mt-2 text-center">
+                        <button @click="Login" class="p-2 duration-200 bg-green-300 rounded-md shadow-md hover:bg-green-600" :disabled="!ServerState" >Login</button>
                     </div>
                     <div class="mt-3">
-                        <p class="text-slate-500 text-sm">Don't have an account yet? <router-link to="/register" class="text-lime-600">Register Now!</router-link> </p>
+                        <p class="text-sm text-slate-500">Don't have an account yet? <router-link to="/register" class="text-lime-600">Register Now!</router-link> </p>
                     </div>
                 </div>
             </div>
@@ -45,7 +45,7 @@ import {verifyServerAvailability, Login} from '@/axios/axiosDefaults'
 
 
 const ServerState = ref(false);
-let ServerResponse = ref(null);
+let ServerResponse = ref(false);
 
 async function Server() {
    ServerResponse.value = await verifyServerAvailability();
--- src/views/Pages/RegisterPage.vue
+++ src/views/Pages/RegisterPage.vue
@@ -1,49 +1,33 @@
-<!-- RegisterPage.vue -->
 <template>
-    <div class="home-background">
-    <div class="registration-form">
-      <h1>Register</h1>
-      <div class="my-2 flex bg-white border p-2 border-slate-200 rounded-lg divide-x divide-slate-300">
-        <UserIcon class="h-6 stroke-slate-300 px-2" />
-        <input v-model="username" type="text" class="w-full px-1" placeholder="Username" />
+  <div class="home-background">
+      <div class="flex items-center justify-center h-full ">
+          <div class="flex w-1/2 bg-white rounded-lg shadow-lg 2xl:h-2/6 xl:h-3/6">
+              <div class="flex items-center w-full h-full p-6">
+                  <div class="flex flex-col w-full">
+                      <div>
+                          <h1>Register</h1>
+                      </div>
+                      <div class="flex p-2 my-2 bg-white border divide-x rounded-lg border-slate-200 divide-slate-300">
+                          <UserIcon class="h-6 px-2 stroke-slate-300"/><input type="text" class="w-full px-1" placeholder="Name" />
+                      </div>
+                      <div class="flex p-2 my-2 bg-white border divide-x rounded-lg border-slate-200 divide-slate-300">
+                          <EnvelopeIcon class="h-6 px-2 stroke-slate-300"/><input type="email" class="w-full px-1" placeholder="Email" />
+                      </div>
+                      <div class="flex p-2 my-2 bg-white border divide-x rounded-lg border-slate-200 divide-slate-300">
+                          <LockClosedIcon class="h-6 px-2 stroke-slate-300"/><input type="password" class="w-full px-1" placeholder="Password"/>
+                      </div>
+                      <div class="mt-2 text-center">
+                          <button @click="Login" class="p-2 duration-200 bg-green-300 rounded-md shadow-md hover:bg-green-600">Register</button>
+                      </div>
+                      <div class="mt-3">
+                          <p class="text-sm text-slate-500">Already registered? <router-link to="/" class="text-lime-600">Login!</router-link> </p>
+                      </div>
+                  </div>
+              </div>
+          </div>
       </div>
-      <div class="my-2 flex bg-white border p-2 border-slate-200 rounded-lg divide-x divide-slate-300">
-        <MailIcon class="h-6 stroke-slate-300 px-2" />
-        <input v-model="email" type="email" class="w-full px-1" placeholder="Email" />
-      </div>
-      <div class="my-2 flex bg-white border p-2 border-slate-200 rounded-lg divide-x divide-slate-300">
-        <LockClosedIcon class="h-6 stroke-slate-300 px-2" />
-        <input v-model="password" type="password" class="w-full px-1" placeholder="Password" />
-      </div>
-      <div class="text-center mt-2">
-        <button @click="register" class="bg-blue-500 p-2 rounded-md shadow-md hover:bg-blue-600 text-white duration-200">Register</button>
-      </div>
-      <div class="mt-3">
-        <p class="text-slate-500 text-sm">Already have an account? <router-link to="home" class="text-blue-600">Login here</router-link></p>
-      </div>
-    </div>
-</div>
+  </div>
   </template>
-  
   <script setup>
-  import { ref } from 'vue';
-  import { UserIcon, LockClosedIcon } from '@heroicons/vue/outline';
-  
-  const username = ref('');
-  const email = ref('');
-  const password = ref('');
-  
-  
+  import { UserIcon, LockClosedIcon, EnvelopeIcon } from '@heroicons/vue/24/outline';
   </script>
-  
-  <style scoped>
-  
-  .registration-form {
-    max-width: 400px;
-    margin: auto;
-    padding: 20px;
-    border: 1px solid #ddd;
-    border-radius: 8px;
-    margin-top: 50px;
-  }
-  </style>
