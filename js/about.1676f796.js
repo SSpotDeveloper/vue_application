a8e82f319e0e12b97c31e0a2ab3d5f0262938202
Manie <subs@softspot.co.za>
2023-12-19 12:37:28 +0200

    adding Dist

---
+++ js/about.1676f796.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvue_application"]=self["webpackChunkvue_application"]||[]).push([[443],{833:function(n,a,u){u.r(a),u.d(a,{default:function(){return p}});var t=u(3396);const e={class:"about"},i=(0,t._)("h1",null,"This is an about page",-1),c=[i];function r(n,a){return(0,t.wg)(),(0,t.iD)("div",e,c)}var s=u(89);const o={},l=(0,s.Z)(o,[["render",r]]);var p=l}}]);
+//# sourceMappingURL=about.1676f796.js.map
