ab8bc544ed6eddf6a08c3ce15f7ebb2d08cbd034
llxErichxll <erichl8855@gmail.com>
2023-12-20 12:21:22 -0800

    AuthenticationPage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
+    <AuthenticatedLayoutView>
     <Suspense>
         <RouterView />
     </Suspense>
+    </AuthenticatedLayoutView>
 </template>
+
+
+<script setup>
+import AuthenticatedLayoutView from '@/views/Pages/AuthenticatedLayoutView/AuthenticatedLayoutView.vue';
+</script>
