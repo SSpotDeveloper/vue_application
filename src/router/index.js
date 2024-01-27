2cb40e96f7df0a53937e26192605e8f6508b4148
Manie <subs@softspot.co.za>
2024-01-27 16:19:52 +0200

    Route Restrictions

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,6 @@
 import { createRouter, createWebHashHistory, } from 'vue-router'
+import {computed} from 'vue';
+import store from  '@/store';
 import HomePageVue from '@/views/Pages/HomePage.vue'
 import RegisterPage from '@/views/Pages/RegisterPage.vue';
 import SettingsPageVue from '@/views/Pages/SettingsPage.vue';
@@ -9,25 +11,39 @@ import DashboardPageVue from '@/views/Pages/DashboardPage.vue';
 import ClientPageVue from '@/views/Pages/ClientPage.vue';
 import InvoicePageVue from '@/views/Pages/Client/InvoicePage.vue';
 
+const Authenticated = computed(() => store.getters.getFakeAuthenticatedState);
+
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: HomePageVue,
+    meta: {
+      Auth: false,
+    }
   },
   { path: '/register',
     name: 'register', 
-    component: RegisterPage 
+    component: RegisterPage,
+    meta: {
+      Auth: false,
+    }
   },
   {
     path: '/dashboard',
     name: 'Dashboard',
     component: DashboardPageVue,
+    meta: {
+      Auth: true,
+    }
   },
   {
     path: '/settings',
     name: 'settings',
     component: SettingsPageVue,
+    meta: {
+      Auth: true,
+    },
     children: [
       {
         path: '',
@@ -43,22 +59,49 @@ const routes = [
     path: '/profile',
     name: 'profile',
     component: ProfilePageVue,
+    meta: {
+      Auth: true,
+    },
   },
   {
     path: '/clients',
     name: 'clients',
     component: ClientPageVue,
+    meta: {
+      Auth: true,
+    },
   },
   {
     path: '/invoice',
     name: 'invoice',
-    component: InvoicePageVue
+    component: InvoicePageVue,
+    meta: {
+      Auth: true,
+    },
   }
 ]
+
+
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.Auth){
+    if(!Authenticated.value){
+      next('/');
+    }else{
+      next();
+    }
+  }else {
+    if(Authenticated.value){
+      next('/dashboard')
+    }else{
+      next();
+    }
+  }
 })
 
 export default router
--- src/views/Pages/HomePage.vue
+++ src/views/Pages/HomePage.vue
@@ -6,13 +6,13 @@
                 <div class="my-1">
                     <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
                     <div class="mt-2">
-                      <input v-model="data.email" type="email" name="email" id="email" class=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="example@yourcompany.co.za" />
+                      <input v-model="data.email" required type="email" name="email" id="email" class=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="example@yourcompany.co.za" />
                     </div>
                 </div>
                 <div class="my-1">
                     <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                     <div class="mt-2">
-                      <input v-model="data.password" type="password" name="password" id="password" class=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                      <input v-model="data.password" required type="password" name="password" id="password" class=" px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
                 <div class="my-3 text-center">
