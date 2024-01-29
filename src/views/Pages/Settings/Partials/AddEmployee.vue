b0db489d6c6e1e0cc309885dd0d475d809724c55
Manie <subs@softspot.co.za>
2024-01-29 14:10:47 +0200

    Update

--- src/views/Pages/Settings/Partials/AddEmployee.vue
+++ src/views/Pages/Settings/Partials/AddEmployee.vue
@@ -2,20 +2,20 @@
     <TransitionRoot as="template" :show="props.state">
       <Dialog as="div" class="relative z-10" @close="closeModal()">
         <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
-          <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div class="fixed inset-0 transition-opacity bg-gray-500 bg-opacity-75" />
         </TransitionChild>
   
         <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div class="flex items-end justify-center min-h-full p-4 text-center sm:items-center sm:p-0">
             <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200" leave-from="opacity-100 translate-y-0 sm:scale-100" leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
-              <DialogPanel class="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+              <DialogPanel class="relative px-4 pt-5 pb-4 overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
                 <div>
-                  <div class="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                    <UserPlusIcon class="h-6 w-6 text-green-600" aria-hidden="true" />
+                  <div class="flex items-center justify-center w-12 h-12 mx-auto bg-green-100 rounded-full">
+                    <UserPlusIcon class="w-6 h-6 text-green-600" aria-hidden="true" />
                   </div>
                   <div class="mt-3 text-center sm:mt-5">
                     <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900">Add Employee</DialogTitle>
-                    <div class="mt-2 flex justify-center">
+                    <div class="flex justify-center mt-2">
                         <div class="w-1/2">
                             <div>
                                 <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
@@ -34,7 +34,7 @@
                                 <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                                 <div class="mt-2">  
                                     <input v-model="data.password" disabled type="text" name="password" id="password" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                                    <span class="text-slate-400">Note:</span><span class="text-slate-400 text-sm">Your employee will receive an email to reset their password on registration.</span>
+                                    <span class="text-slate-400">Note:</span><span class="text-sm text-slate-400">Your employee will receive an email to reset their password on registration.</span>
                                 </div>
                             </div>
                         </div>
@@ -42,8 +42,8 @@
                   </div>
                 </div>
                 <div class="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
-                  <button type="button" class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2" @click="closeModal()">Deactivate</button>
-                  <button type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0" @click="closeModal()" ref="cancelButtonRef">Cancel</button>
+                  <button type="button" class="inline-flex justify-center w-full px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2" @click="closeModal()">Deactivate</button>
+                  <button type="button" class="inline-flex justify-center w-full px-3 py-2 mt-3 text-sm font-semibold text-gray-900 bg-white rounded-md shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0" @click="closeModal()" ref="cancelButtonRef">Cancel</button>
                 </div>
               </DialogPanel>
             </TransitionChild>
